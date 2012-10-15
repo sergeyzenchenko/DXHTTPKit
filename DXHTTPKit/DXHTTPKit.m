5b2ae307c7bfd347b5167f9f806e3969f08b52cf
Sergey Zenchenko <s.zenchenko@111min.com>
2012-10-15 22:23:43 +0300

    add base project

---
+++ DXHTTPKit/DXHTTPKit.m
@@ -0,0 +1,13 @@
+//
+//  DXHTTPKit.m
+//  DXHTTPKit
+//
+//  Created by Sergey Zenchenko on 10/15/12.
+//  Copyright (c) 2012 111Minutes. All rights reserved.
+//
+
+#import "DXHTTPKit.h"
+
+@implementation DXHTTPKit
+
+@end
